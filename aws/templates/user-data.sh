e316e5cf5a3b5989d3821e934692d751cd012e38
jakub.coufal <jakub.coufal@wandera.com>
2019-07-02 10:49:21 +0200

    S3 backup unit

--- aws/templates/user-data.sh
+++ aws/templates/user-data.sh
@@ -53,7 +53,7 @@ After=docker.service
 Type=simple
 User=rancher
 Group=rancher
-ExecStartPre=/bin/bash -c """/usr/bin/docker container inspect rancher 2> /dev/null || /usr/bin/docker run -d --name=rancher --restart=on-failure -p ${HTTPS_PORT}:${HTTPS_PORT} -p ${ETCD_PORT}:${ETCD_PORT} -v ${RANCHER_DIR}:/var/lib/rancher ${RANCHER_IMAGE} --https-listen-port=${HTTPS_PORT} --no-cacerts"""
+ExecStartPre=/bin/bash -c """/usr/bin/docker container inspect rancher 2> /dev/null || /usr/bin/docker run -d --name=rancher --restart=on-failure -p $HTTPS_PORT:$HTTPS_PORT -p $ETCD_PORT:$ETCD_PORT -v $RANCHER_DIR:/var/lib/rancher $RANCHER_IMAGE --https-listen-port=$HTTPS_PORT --no-cacerts"""
 ExecStart=/usr/bin/docker start -a rancher
 ExecReload=/usr/bin/docker stop -t 30 rancher
 
@@ -74,7 +74,7 @@ node_exporter_setup() {
   wget -qO - https://github.com/prometheus/node_exporter/releases/download/v$NODE_EXPORTER_VER/node_exporter-$NODE_EXPORTER_VER.linux-amd64.tar.gz | tar xvzC /tmp
   mv /tmp/node_exporter-$NODE_EXPORTER_VER.linux-amd64/node_exporter /usr/local/bin/
 
-  node_exporter  --version
+  node_exporter --version
 
   COLLECTORS=""
   for it in ${node_exporter_collectors}; do
@@ -109,8 +109,63 @@ EOF
   ufw allow $NODE_EXPORTER_PORT
 }
 
+etcd_backup_setup () {
+
+  echo "Installing awsless"
+  curl https://raw.githubusercontent.com/wallix/awless/master/getawless.sh | bash
+  mv awless /usr/local/bin/
+
+  awless --version
+
+  cat > /etc/systemd/system/etcdbackup.timer << EOF
+[Unit]
+Description=etcd backup timer
+Requires=etcdbackup.service
+Wants=network-online.target docker.socket
+After=docker.service
+
+[Timer]
+Unit=etcdbackup.service
+OnCalendar=${s3_backup_schedule}
+Persistent=true
+
+SyslogIdentifier=etcdbackup
+
+[Install]
+WantedBy=timers.target
+EOF
+  cat > /etc/systemd/system/etcdbackup.service << EOF
+[Unit]
+Description=etcd backup service
+Wants=network-online.target docker.socket etcdbackup.timer
+After=docker.service
+
+[Service]
+Environment=AWS_ACCESS_KEY_ID=${s3_backup_key}
+Environment=AWS_SECRET_ACCESS_KEY=${s3_backup_secret}
+ExecStart=/bin/bash -c /usr/local/bin/backup_etcd
+
+SyslogIdentifier=etcdbackup
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  cat > /usr/local/bin/backup_etcd << EOF
+  /usr/local/bin/awless --aws-region=${s3_backup_region} --force create s3object bucket=${s3_backup_bucket} name=backups/snapshot-\$(date +%Y-%m-%d-%H%M).db file=$RANCHER_DIR/management-state/etcd/member/snap/db
+EOF
+  chmod +x /usr/local/bin/backup_etcd
+
+  systemctl start etcdbackup
+  systemctl enable etcdbackup
+}
+
 # Main section
 filesystem_setup
 docker_setup
 rancher_setup
 node_exporter_setup
+
+if [[ -n "${s3_backup_region}" && -n "${s3_backup_bucket}" ]]; then
+  etcd_backup_setup
+fi
